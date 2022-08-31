e4faf5b648d10430bf668b1009ad2f2b6c3c4f41
gjouetpastre <gjouetpastre@ecoceatechnologies.com>
2022-08-31 16:31:35 +0200

    add manifest.json and use https

--- src/mock-database.ts
+++ src/mock-database.ts
@@ -5,600 +5,600 @@ export const products: Product[] = [
 		id: '1',
 		name: 'Mushroom - Porcini Frozen',
 		price: 6854,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '2',
 		name: 'Pasta - Tortellini, Fresh',
 		price: 4885,
-		img: 'http://dummyimage.com/152x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/152x100.png/cc0000/ffffff'
 	},
 	{
 		id: '3',
 		name: 'Beef Ground Medium',
 		price: 2834,
-		img: 'http://dummyimage.com/168x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/168x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '4',
 		name: 'Sauce - Soya, Dark',
 		price: 8009,
-		img: 'http://dummyimage.com/105x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/105x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '5',
 		name: 'Cheese - St. Paulin',
 		price: 8687,
-		img: 'http://dummyimage.com/105x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/105x100.png/cc0000/ffffff'
 	},
 	{
 		id: '6',
 		name: 'Jack Daniels',
 		price: 7493,
-		img: 'http://dummyimage.com/134x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/134x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '7',
 		name: 'Pork - Smoked Kassler',
 		price: 8248,
-		img: 'http://dummyimage.com/221x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/221x100.png/ff4444/ffffff'
 	},
 	{
 		id: '8',
 		name: 'Macaroons - Homestyle Two Bit',
 		price: 2096,
-		img: 'http://dummyimage.com/198x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/198x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '9',
 		name: 'Seabream Whole Farmed',
 		price: 8495,
-		img: 'http://dummyimage.com/117x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/117x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '10',
 		name: 'Ice Cream Bar - Hageen Daz To',
 		price: 3601,
-		img: 'http://dummyimage.com/165x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/165x100.png/ff4444/ffffff'
 	},
 	{
 		id: '11',
 		name: 'Corn - Mini',
 		price: 9754,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '12',
 		name: 'Lettuce Romaine Chopped',
 		price: 260,
-		img: 'http://dummyimage.com/172x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/172x100.png/dddddd/000000'
 	},
 	{
 		id: '13',
 		name: 'Leeks - Baby, White',
 		price: 611,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '14',
 		name: 'Pork Loin Cutlets',
 		price: 7072,
-		img: 'http://dummyimage.com/211x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/211x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '15',
 		name: 'Pears - Bartlett',
 		price: 1955,
-		img: 'http://dummyimage.com/134x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/134x100.png/cc0000/ffffff'
 	},
 	{
 		id: '16',
 		name: 'Pepper - Sorrano',
 		price: 9012,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '17',
 		name: 'Pork - Back, Short Cut, Boneless',
 		price: 2345,
-		img: 'http://dummyimage.com/241x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/241x100.png/cc0000/ffffff'
 	},
 	{
 		id: '18',
 		name: 'Figs',
 		price: 9305,
-		img: 'http://dummyimage.com/194x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/194x100.png/cc0000/ffffff'
 	},
 	{
 		id: '19',
 		name: 'Apricots - Halves',
 		price: 6593,
-		img: 'http://dummyimage.com/188x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/188x100.png/dddddd/000000'
 	},
 	{
 		id: '20',
 		name: 'Star Anise, Whole',
 		price: 1285,
-		img: 'http://dummyimage.com/178x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/178x100.png/ff4444/ffffff'
 	},
 	{
 		id: '21',
 		name: 'Wine - Sogrape Mateus Rose',
 		price: 202,
-		img: 'http://dummyimage.com/173x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/173x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '22',
 		name: 'Appetizer - Chicken Satay',
 		price: 7793,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '23',
 		name: 'Wine - Puligny Montrachet A.',
 		price: 5482,
-		img: 'http://dummyimage.com/181x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/181x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '24',
 		name: 'Capon - Breast, Wing On',
 		price: 2867,
-		img: 'http://dummyimage.com/120x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/120x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '25',
 		name: 'Bread - Multigrain',
 		price: 4318,
-		img: 'http://dummyimage.com/212x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/212x100.png/cc0000/ffffff'
 	},
 	{
 		id: '26',
 		name: 'Fruit Salad Deluxe',
 		price: 8131,
-		img: 'http://dummyimage.com/215x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/215x100.png/cc0000/ffffff'
 	},
 	{
 		id: '27',
 		name: 'Wonton Wrappers',
 		price: 5007,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '28',
 		name: 'Wine - Jaboulet Cotes Du Rhone',
 		price: 705,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '29',
 		name: 'Dip - Tapenade',
 		price: 8841,
-		img: 'http://dummyimage.com/106x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/106x100.png/cc0000/ffffff'
 	},
 	{
 		id: '30',
 		name: 'Juice - V8 Splash',
 		price: 6354,
-		img: 'http://dummyimage.com/129x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/129x100.png/dddddd/000000'
 	},
 	{
 		id: '31',
 		name: 'Chocolate - Liqueur Cups With Foil',
 		price: 6973,
-		img: 'http://dummyimage.com/222x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/222x100.png/ff4444/ffffff'
 	},
 	{
 		id: '32',
 		name: 'Easy Off Oven Cleaner',
 		price: 5775,
-		img: 'http://dummyimage.com/242x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/242x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '33',
 		name: 'Pate - Cognac',
 		price: 7233,
-		img: 'http://dummyimage.com/183x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/183x100.png/ff4444/ffffff'
 	},
 	{
 		id: '34',
 		name: 'Butcher Twine 4r',
 		price: 966,
-		img: 'http://dummyimage.com/183x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/183x100.png/ff4444/ffffff'
 	},
 	{
 		id: '35',
 		name: 'Vodka - Hot, Lnferno',
 		price: 7868,
-		img: 'http://dummyimage.com/122x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/122x100.png/dddddd/000000'
 	},
 	{
 		id: '36',
 		name: 'Beans - Soya Bean',
 		price: 795,
-		img: 'http://dummyimage.com/161x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/161x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '37',
 		name: 'Squid - U 5',
 		price: 2454,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '38',
 		name: 'Salt - Sea',
 		price: 8649,
-		img: 'http://dummyimage.com/105x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/105x100.png/dddddd/000000'
 	},
 	{
 		id: '39',
 		name: 'Water - Evian 355 Ml',
 		price: 2815,
-		img: 'http://dummyimage.com/158x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/158x100.png/dddddd/000000'
 	},
 	{
 		id: '40',
 		name: 'Propel Sport Drink',
 		price: 1881,
-		img: 'http://dummyimage.com/216x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/216x100.png/ff4444/ffffff'
 	},
 	{
 		id: '41',
 		name: 'Pie Filling - Cherry',
 		price: 2064,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '42',
 		name: 'Lamb Rack - Ontario',
 		price: 8294,
-		img: 'http://dummyimage.com/233x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/233x100.png/ff4444/ffffff'
 	},
 	{
 		id: '43',
 		name: 'Cheese - Grie Des Champ',
 		price: 6496,
-		img: 'http://dummyimage.com/105x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/105x100.png/ff4444/ffffff'
 	},
 	{
 		id: '44',
 		name: 'Quinoa',
 		price: 6522,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '45',
 		name: 'Beef - Prime Rib Aaa',
 		price: 7086,
-		img: 'http://dummyimage.com/235x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/235x100.png/dddddd/000000'
 	},
 	{
 		id: '46',
 		name: 'Amaretto',
 		price: 2779,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '47',
 		name: 'Scallops - In Shell',
 		price: 1952,
-		img: 'http://dummyimage.com/218x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/218x100.png/ff4444/ffffff'
 	},
 	{
 		id: '48',
 		name: 'Tomatoes Tear Drop Yellow',
 		price: 6518,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '49',
 		name: 'Tea - Darjeeling, Azzura',
 		price: 608,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '50',
 		name: 'Pepperoni Slices',
 		price: 1562,
-		img: 'http://dummyimage.com/182x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/182x100.png/cc0000/ffffff'
 	},
 	{
 		id: '51',
 		name: 'Salmon - Smoked, Sliced',
 		price: 9772,
-		img: 'http://dummyimage.com/157x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/157x100.png/dddddd/000000'
 	},
 	{
 		id: '52',
 		name: 'Wine - Chardonnay South',
 		price: 5249,
-		img: 'http://dummyimage.com/167x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/167x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '53',
 		name: 'Filo Dough',
 		price: 2563,
-		img: 'http://dummyimage.com/121x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/121x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '54',
 		name: 'Oil - Sunflower',
 		price: 3137,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '55',
 		name: 'Dried Apple',
 		price: 8753,
-		img: 'http://dummyimage.com/176x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/176x100.png/dddddd/000000'
 	},
 	{
 		id: '56',
 		name: 'Mahi Mahi',
 		price: 4955,
-		img: 'http://dummyimage.com/144x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/144x100.png/dddddd/000000'
 	},
 	{
 		id: '57',
 		name: 'Bread - Italian Sesame Poly',
 		price: 8091,
-		img: 'http://dummyimage.com/138x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/138x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '58',
 		name: 'Sugar - Palm',
 		price: 484,
-		img: 'http://dummyimage.com/215x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/215x100.png/dddddd/000000'
 	},
 	{
 		id: '59',
 		name: 'Glucose',
 		price: 3313,
-		img: 'http://dummyimage.com/225x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/225x100.png/cc0000/ffffff'
 	},
 	{
 		id: '60',
 		name: 'Lumpfish Black',
 		price: 4461,
-		img: 'http://dummyimage.com/241x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/241x100.png/dddddd/000000'
 	},
 	{
 		id: '61',
 		name: 'Wine - Winzer Krems Gruner',
 		price: 6933,
-		img: 'http://dummyimage.com/113x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/113x100.png/cc0000/ffffff'
 	},
 	{
 		id: '62',
 		name: 'Bagel - Everything',
 		price: 8807,
-		img: 'http://dummyimage.com/145x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/145x100.png/cc0000/ffffff'
 	},
 	{
 		id: '63',
 		name: 'Rabbit - Frozen',
 		price: 2071,
-		img: 'http://dummyimage.com/126x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/126x100.png/ff4444/ffffff'
 	},
 	{
 		id: '64',
 		name: 'Honey - Comb',
 		price: 5892,
-		img: 'http://dummyimage.com/182x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/182x100.png/dddddd/000000'
 	},
 	{
 		id: '65',
 		name: 'Wine - Fontanafredda Barolo',
 		price: 412,
-		img: 'http://dummyimage.com/210x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/210x100.png/ff4444/ffffff'
 	},
 	{
 		id: '66',
 		name: 'Beer - Labatt Blue',
 		price: 1329,
-		img: 'http://dummyimage.com/186x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/186x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '67',
 		name: 'Cheese - Brick With Pepper',
 		price: 6114,
-		img: 'http://dummyimage.com/198x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/198x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '68',
 		name: 'Bread Base - Gold Formel',
 		price: 4879,
-		img: 'http://dummyimage.com/176x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/176x100.png/dddddd/000000'
 	},
 	{
 		id: '69',
 		name: 'Cup - Translucent 7 Oz Clear',
 		price: 2486,
-		img: 'http://dummyimage.com/109x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/109x100.png/cc0000/ffffff'
 	},
 	{
 		id: '70',
 		name: 'Roe - Lump Fish, Red',
 		price: 8866,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '71',
 		name: 'Rice - Long Grain',
 		price: 2369,
-		img: 'http://dummyimage.com/234x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/234x100.png/ff4444/ffffff'
 	},
 	{
 		id: '72',
 		name: 'Flower - Leather Leaf Fern',
 		price: 7442,
-		img: 'http://dummyimage.com/107x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/107x100.png/dddddd/000000'
 	},
 	{
 		id: '73',
 		name: 'Galliano',
 		price: 1137,
-		img: 'http://dummyimage.com/248x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/248x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '74',
 		name: 'Knife Plastic - White',
 		price: 3953,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '75',
 		name: 'Cups 10oz Trans',
 		price: 130,
-		img: 'http://dummyimage.com/231x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/231x100.png/ff4444/ffffff'
 	},
 	{
 		id: '76',
 		name: 'Chicken - Whole',
 		price: 1877,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '77',
 		name: 'Ice Cream - Chocolate',
 		price: 867,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '78',
 		name: 'Salmon Steak - Cohoe 8 Oz',
 		price: 7002,
-		img: 'http://dummyimage.com/144x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/144x100.png/ff4444/ffffff'
 	},
 	{
 		id: '79',
 		name: 'Syrup - Monin - Granny Smith',
 		price: 2092,
-		img: 'http://dummyimage.com/236x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/236x100.png/dddddd/000000'
 	},
 	{
 		id: '80',
 		name: 'Vermouth - Sweet, Cinzano',
 		price: 9935,
-		img: 'http://dummyimage.com/125x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/125x100.png/dddddd/000000'
 	},
 	{
 		id: '81',
 		name: 'Tobasco Sauce',
 		price: 2045,
-		img: 'http://dummyimage.com/189x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/189x100.png/ff4444/ffffff'
 	},
 	{
 		id: '82',
 		name: 'Napkin - Cocktail,beige 2 - Ply',
 		price: 537,
-		img: 'http://dummyimage.com/241x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/241x100.png/dddddd/000000'
 	},
 	{
 		id: '83',
 		name: 'Flour - Whole Wheat',
 		price: 9627,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '84',
 		name: 'Bread - Kimel Stick Poly',
 		price: 3319,
-		img: 'http://dummyimage.com/248x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/248x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '85',
 		name: 'Grand Marnier',
 		price: 8913,
-		img: 'http://dummyimage.com/116x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/116x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '86',
 		name: 'Pur Source',
 		price: 976,
-		img: 'http://dummyimage.com/150x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/150x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '87',
 		name: 'Langers - Ruby Red Grapfruit',
 		price: 5463,
-		img: 'http://dummyimage.com/185x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/185x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '88',
 		name: 'Beef Ground Medium',
 		price: 7306,
-		img: 'http://dummyimage.com/134x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/134x100.png/ff4444/ffffff'
 	},
 	{
 		id: '89',
 		name: 'Wine - Touraine Azay - Le - Rideau',
 		price: 7612,
-		img: 'http://dummyimage.com/142x100.png/ff4444/ffffff'
+		img: 'https://dummyimage.com/142x100.png/ff4444/ffffff'
 	},
 	{
 		id: '90',
 		name: 'Wine - White, Cooking',
 		price: 2127,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '91',
 		name: 'Cucumber - English',
 		price: 385,
-		img: 'http://dummyimage.com/104x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/104x100.png/dddddd/000000'
 	},
 	{
 		id: '92',
 		name: 'Thermometer Digital',
 		price: 8391,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '93',
 		name: 'Jameson Irish Whiskey',
 		price: 5832,
-		img: 'http://dummyimage.com/130x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/130x100.png/cc0000/ffffff'
 	},
 	{
 		id: '94',
 		name: 'Appetizer - Asian Shrimp Roll',
 		price: 6196,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '95',
 		name: 'Tomatoes - Cherry, Yellow',
 		price: 925,
-		img: 'http://dummyimage.com/203x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/203x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '96',
 		name: 'Pastry - Raisin Muffin - Mini',
 		price: 3279,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '97',
 		name: 'Lettuce - Red Leaf',
 		price: 6988,
-		img: 'http://dummyimage.com/116x100.png/cc0000/ffffff'
+		img: 'https://dummyimage.com/116x100.png/cc0000/ffffff'
 	},
 	{
 		id: '98',
 		name: 'Ocean Spray - Ruby Red',
 		price: 1978,
-		img: 'http://dummyimage.com/223x100.png/5fa2dd/ffffff'
+		img: 'https://dummyimage.com/223x100.png/5fa2dd/ffffff'
 	},
 	{
 		id: '99',
 		name: 'Cookies Cereal Nut',
 		price: 3109,
-		img: 'http://dummyimage.com/171x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/171x100.png/dddddd/000000'
 	},
 	{
 		id: '100',
 		name: 'Cake - Cake Sheet Macaroon',
 		price: 8532,
-		img: 'http://dummyimage.com/140x100.png/dddddd/000000'
+		img: 'https://dummyimage.com/140x100.png/dddddd/000000'
 	}
 ];
